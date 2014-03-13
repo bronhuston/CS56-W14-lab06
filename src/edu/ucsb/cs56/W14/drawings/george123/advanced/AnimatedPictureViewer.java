ed91e3bcaad8b722e54b8955728300ca12420146
George Chen <george@umail.ucsb.edu>
2014-03-13 14:42:31 -0700

    Updated

--- src/edu/ucsb/cs56/W14/drawings/george123/advanced/AnimatedPictureViewer.java
+++ src/edu/ucsb/cs56/W14/drawings/george123/advanced/AnimatedPictureViewer.java
@@ -8,7 +8,7 @@ public class AnimatedPictureViewer {
 
     private DrawPanel panel = new DrawPanel();
 
-    private Pokeball ipod = new Pokeball(200, 200, 200);
+    private Pokeball pokeball = new Pokeball(200, 200, 200);
 
     Thread anim;
 
@@ -57,7 +57,7 @@ public class AnimatedPictureViewer {
           g2.setColor(Color.white);
           g2.fillRect(0,0,this.getWidth(), this.getHeight());
 
-          // Draw the Ipod
+          // Draw the Pokeball
           g2.setColor(Color.RED);
           Pokeball test = new Pokeball(x, y, 200);
           g2.draw(test);
